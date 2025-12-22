aa5610040189bfe455c301e1312ef276b6ef3941
M <lmiguelvargasf@gmail.com>
2025-12-21 22:02:27 -0500

    Simplify database for testing

--- backend/src/backend/config/base.py
+++ backend/src/backend/config/base.py
@@ -1,14 +1,19 @@
+from functools import cached_property
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     postgres_db: str
-    postgres_test_db: str
     postgres_user: str
     postgres_password: str
     postgres_host: str
     postgres_port: int
     admin_session_secret: str
+
+    @cached_property
+    def postgres_test_db(self) -> str:
+        return f"{self.postgres_db}_test"
 
 
 settings = Settings()  # type: ignore[call-arg]
--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,13 +1,12 @@
 -- The database named in the POSTGRES_DB environment variable
 -- is created automatically by the Docker entrypoint.
--- This script only handles creation of the test database specified by POSTGRES_TEST_DB.
--- Abort on any error in this script
+-- This script creates the test database by appending _test to POSTGRES_DB.
 \set ON_ERROR_STOP on
-\getenv test_db POSTGRES_TEST_DB
+\getenv main_db POSTGRES_DB
 SELECT
-  format('CREATE DATABASE %I', :'test_db')
+  format('CREATE DATABASE %I', :'main_db' || '_test')
 WHERE NOT EXISTS (
   SELECT 1
     FROM pg_database
-   WHERE datname = :'test_db'
+   WHERE datname = :'main_db' || '_test'
 )\gexec
