b1316f114c19701d20cf2d6fb84c3fbf69adf00b
M <lmiguelvargasf@gmail.com>
2025-12-22 11:32:24 -0500

    Remove needless __all__

--- backend/src/backend/apps/models.py
+++ backend/src/backend/apps/models.py
@@ -1,7 +1,5 @@
 from advanced_alchemy.base import metadata_registry
 
-from backend.apps.users.models import UserModel
+from backend.apps.users.models import UserModel as UserModel
 
 metadata = metadata_registry[None]
-
-__all__ = ["UserModel", "metadata"]
