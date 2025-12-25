8513fd87b5e20ba413687c181c518696f065fbd9
M <lmiguelvargasf@gmail.com>
2025-12-25 10:07:35 -0500

    chore: generalize services graphql context and simplify type (#54)
    * feat: add GraphQL context and services for user management
    
    * refactor: update user mutation to utilize GraphQL context and services
    
    * refactor: update user query to use GraphQL context for service access
    
    * refactor: streamline GraphQL context to include services and update tests accordingly
    
    * Improve typing

---
+++ backend/src/backend/graphql/context.py
@@ -0,0 +1,20 @@
+from functools import cached_property
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from strawberry.litestar.controller import BaseContext
+
+from backend.apps.users.services import UserService
+
+
+class Services:
+    def __init__(self, db_session: AsyncSession) -> None:
+        self._db_session = db_session
+
+    @cached_property
+    def users(self) -> UserService:
+        return UserService(self._db_session)
+
+
+class GraphQLContext(BaseContext, kw_only=True):
+    db_session: AsyncSession
+    services: Services
