6f623caec6a2c6ef667bcc5ddba4f9c718948db8
M <lmiguelvargasf@gmail.com>
2026-01-18 16:45:22 -0500

    Make sure mise is trusted

--- setup.sh
+++ setup.sh
@@ -97,7 +97,7 @@ main() {
   trap cleanup EXIT
 
   info "Installing toolchain..."
-  "${MISE_BIN}" install -y
+  "${MISE_BIN}" trust && "${MISE_BIN}" install -y
 
   info "Installing pre-commit hooks..."
   "${MISE_BIN}" exec -- pre-commit install || die "pre-commit install failed. Re-run with: mise exec -- pre-commit install"
