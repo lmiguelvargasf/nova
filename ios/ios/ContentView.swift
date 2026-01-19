c084a23fba1ecf8c17563c12b2b37f9522c1ce96
M <lmiguelvargasf@gmail.com>
2026-01-19 12:24:50 -0500

    Minor UI

--- ios/ios/ContentView.swift
+++ ios/ios/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "gearshape.2.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Nova 🌟")
         }
         .padding()
     }
