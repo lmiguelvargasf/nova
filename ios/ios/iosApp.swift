5359a65f85323932cfb40d3f8b810d5cc7d6ad9b
M <lmiguelvargasf@gmail.com>
2026-01-19 12:02:04 -0500

    Create ios app

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by M on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
