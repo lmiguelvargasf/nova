706366496364508e2d05aa5f516ca054a4cbe033
M <lmiguelvargasf@gmail.com>
2026-01-19 19:40:37 -0500

    Solve url issue

--- ios/ios/Core/Configuration/APIConfiguration.swift
+++ ios/ios/Core/Configuration/APIConfiguration.swift
@@ -2,12 +2,12 @@ import Foundation
 
 struct APIConfiguration: Sendable {
     let baseURL: URL
+    private static let defaultBaseURL = URL(string: "http://localhost:8000")!
 
     static func load(bundle: Bundle = .main) -> APIConfiguration {
         let rawValue = bundle.object(forInfoDictionaryKey: "API_BASE_URL") as? String
         let configuredURL = URL(string: rawValue ?? "")
-        let fallbackURL = URL(string: "http://localhost:8000")
-        let resolvedURL = configuredURL ?? fallbackURL ?? URL(fileURLWithPath: "/")
+        let resolvedURL = configuredURL ?? defaultBaseURL
         return APIConfiguration(baseURL: resolvedURL)
     }
 }
